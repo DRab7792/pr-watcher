c3ba804287f90b526e3a1f2aae7114482ee407d2
Dan Rabinowitz <dan.a.rabinowitz@gmail.com>
2018-08-16 23:17:41 -0400

    Adding express server

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const express = require('express');
+const express = require('express'),
+    config = require('./config'),
+    githubAuth = require('github-oauth');
 
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+
+app.get('/', (req, res) => {
+    return res.send(process.env.CLIENT_ID);
+});
+
+app.listen(port, () => {
+    console.log("App started on port " + port);
+});
