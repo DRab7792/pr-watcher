9e037909b03e6b54e992325f86c983186cdcab73
Dan Rabinowitz <dan.a.rabinowitz@gmail.com>
2018-08-18 17:08:26 -0400

    Finishing home page

--- public/js/index.js
+++ public/js/index.js
@@ -36,8 +36,8 @@ $(document).ready(function() {
                 $('.loggedIn').show();
             });
         }else{
-            console.log("User is not logged in");
-            $('.loggedOut').show();
+            console.log("User is not logged in", $('.loggedOut'));
+            $('.loggedOut').css('display', 'block');
             $('.loggedIn').hide();
         }
     }
