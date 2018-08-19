30a7dd770e2a3d2ff0e38713985c0e0e15c945ce
Dan Rabinowitz <dan.a.rabinowitz@gmail.com>
2018-08-18 22:30:18 -0400

    Building out pr endpoint

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express'),
     request = require('request'),
     queryString = require('query-string'),
     cookieParser = require('cookie-parser'),
+    async = require('async'),
     config = require('./config');
 
 const githubAuth = require('github-oauth')({
@@ -65,9 +66,10 @@ function callGithubApi(req, res, route, params, callback) {
         json: true,
         headers: {
             'User-Agent': 'PR Watcher',
-            'Authorization': 'token ' + req.cookies.token
+            'Authorization': 'token ' + req.cookies.token,
         }
     }, (err, resp, body) => {
+        console.log("Rate Limit Remaining: " + resp.headers['x-ratelimit-remaining']);
         if (err){
             return res.send(formResponse(false, [], err));
         }
@@ -88,6 +90,38 @@ app.get('/api/repos', (req, res) => {
     callGithubApi(req, res, "user/repos", {"sort": "updated", "per_page": 100});
 });
 
+// Get review comments
+const getReviewCommentsFromPRs = (req, res, prs) => {
+    return new Promise((resolve, reject) => {
+        async.eachOf(prs, (cur, i, done) => {
+            callGithubApi(req, res, cur.review_comments_url.replace("https://api.github.com/", ""), null, (data) => {
+                prs[i].review_comments = data;
+                return done();
+            });
+        }, () => {
+            return resolve(prs);
+        });
+    });
+};
+
+// Get issue comments
+const getCommentsFromPRs = (req, res, prs) => {
+    return new Promise((resolve, reject) => {
+        async.eachOf(prs, (cur, i, done) => {
+            callGithubApi(req, res, cur.comments_url.replace("https://api.github.com/", ""), null, (data) => {
+                prs[i].comments = data;
+                return done();
+            });
+        }, () => {
+            return resolve(prs);
+        });
+    });
+};
+
+const resolvePr = pr => {
+    return pr;
+};
+
 app.get('/api/prs', (req, res) => {
     checkToken(req, res);
 
@@ -103,16 +137,14 @@ app.get('/api/prs', (req, res) => {
     // Setup arrays
     const finalData = {
         'mine': {
-            'need-review': [],
+            'needs-review': [],
             'in-review':[]
         },
         'others': {
-            'need-review': [],
+            'needs-review': [],
             'in-review': []
         }
     };
-
-    const mine = [], others = [];
 
     // Get the prs
     callGithubApi(req, res, "repos/" + req.query.owner + "/" + req.query.repo + "/pulls", {
@@ -121,18 +153,32 @@ app.get('/api/prs', (req, res) => {
         "direction": "asc",
         "per_page": 100
     }, prs => {
-        prs.forEach(cur => {
-            if (cur.user.id == req.query.user_id) {
-                mine.push(cur);
-            } else {
-                others.push(cur);
-            }
-        });
+        // Get comments for all PRs
+        getReviewCommentsFromPRs(req, res, prs)
+        .then(prs => {
+            return getCommentsFromPRs(req, res, prs);
+        })
+        .then(prs => {
+            // Categorize PRs
+            prs.forEach(cur => {
+                const adjPr = resolvePr(cur);
+                if (cur.user.id == req.query.user_id) {
+                    if (cur.comments.length) {
+                        finalData.mine['in-review'].push(adjPr);
+                    } else {
+                        finalData.mine['needs-review'].push(adjPr);
+                    }
+                } else {
+                    if (cur.comments.length) {
+                        finalData.others['in-review'].push(adjPr);
+                    } else {
+                        finalData.others['needs-review'].push(adjPr);
+                    }
+                }
+            });
 
-        return res.send(formResponse(true, {
-            'mine': mine,
-            'others': others
-        }));
+            return res.send(formResponse(true, finalData));
+        });
     });
 });
 
